60a542ea4f1ce4f999542486e9e879bd9a3033eb
vpalmerio <vpalmerio@protonmail.com>
2025-02-27 11:24:31 -0700

    Improves types in orderbook plugin

--- packages/plugin-ai-orderbook/src/types.ts
+++ packages/plugin-ai-orderbook/src/types.ts
@@ -4,6 +4,24 @@ export interface LatestBlockHeightResponse {
 
 export type Order = [string, number, number, string, string, number, number, number, string, string];
 
+export enum OrderIndexes {
+    User = 0,
+    AmountOwned,
+    AmountToBuy,
+    TokenAddressToBuy,
+    TokenAddressHeld,
+    Slippage,
+    OrderPlaced,
+    OrderMatched,
+    PartiallyFulfilled,
+    Fulfilled
+}
+
+export interface MinRatioTradeResponse {
+    tokenAddr: string,
+    ratio: number
+}
+
 export interface GetAllOrdersResponse {
-    orders: Object[]
+    orders: Order[]
 }
