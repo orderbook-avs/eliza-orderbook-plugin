399e92f775137437515ae60972bc399c4b9e8e18
vpalmerio <vpalmerio@protonmail.com>
2025-02-27 12:12:36 -0700

    Fix callback msg in getBestPrice action

--- packages/plugin-ai-orderbook/src/actions/getBestPrice.ts
+++ packages/plugin-ai-orderbook/src/actions/getBestPrice.ts
@@ -30,7 +30,7 @@ export const getBestPriceAction: Action = {
             if (callback) {
                 
                 callback({
-                    text: `Here are all of the orders that have occurred:\n
+                    text: `Here are the most recent 'best' prices:\n
                     
                     Token A: ${response[0].tokenAddr}\n
                     What 1 token B might get you: ${response[0].ratio}
